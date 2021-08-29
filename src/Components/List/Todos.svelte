2b23fb39d19a4b87d348940152190342e9ee61af
Mrbeyond <beyondbeyond602@gmail.com>
2021-08-29 06:56:58 +0100

    Trying to fixed table and row stretching the whole length of the screen

--- src/Components/List/Todos.svelte
+++ src/Components/List/Todos.svelte
@@ -1,32 +1,72 @@
 <script>
-  import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
+  import DataTable, { Head, Body, Row, Cell, Label } from '@smui/data-table';
   import { mapTodo } from '../../Utilities/storage';
-  import AddTodo from '../Form/AddTodo.svelte';
-  import MdDeleteForever from 'svelte-icons/md/MdDeleteForever.svelte';
-  import FaEdit from 'svelte-icons/fa/FaEdit.svelte'
+  import AddTodo from '../Form/AddTodo.svelte';  
+  import Fa from 'svelte-fa'
+  import { faFlag } from '@fortawesome/free-solid-svg-icons'
+
+  import SvelteTable from "svelte-table";
+  
   let heads = ['S/N', 'Title', 'Details', 'Time', 'Status', 'Actions' ];
-  let sizze = [5, 15, 40, 15, 10, 15]
+  let sizze = [5, 15, 40, 15, 10, 15];
+  // let ids = ["title","details","date","status"];
+
+  const getIndex=(obejct, data)=>{
+    return Object.keys(obejct).findIndex(e=>e == data);
+  }
   
   let todos = mapTodo();
   $: console.log(todos);
-
+  const columns = [
+    /** columns config (example below) */
+    {
+      key: "id",
+      title: "S/N",
+      value: v => v.id
+    },
+    {
+      key: "title",
+      title: "Tile",
+      value: (v, i) => v.title +" == "+ i
+    },
+    {
+      key: "details",
+      title: "Details",
+      value: v => v.details
+    },
+    {
+      key: "status",
+      title: "Status",
+      value: v => v.status
+    },
+  ];
 </script>
 
 
-<div>
+
+<div class="box">
   <AddTodo />
-  <FaEdit />
+ 
+  <!-- <SvelteTable classNameTable="box" 
+  classNameCell="cells"
+  columns="{columns}" rows="{todos}">
+      
+  </SvelteTable> -->
   <nav class="red">
   </nav>
-  <div style=" background:red">
+  <div class="icon">
+  </div>
+  <div style=" background:skyblue; display:flex; justify-content:stretch">
     <DataTable
       table$aria-label="Todo list"
       style="width: 100%;"
     >
       <Head>
-        <Row>
+        <Row style="background:red">
           {#each heads as head, i }
-            <Cell style="width:{sizze[i]}%; text-align:start ">{head}</Cell>
+
+            <Cell style="" columnId={head} ><Label>{head}</Label></Cell>
+            <!-- style="width:{sizze[i]}%;"  style={i ==2 && 'width: 100%'}-->
           {/each}
         </Row>
       </Head>
@@ -34,14 +74,18 @@
         {#each todos as todo, index}
           <Row>
             <Cell>{index+1}</Cell>
-            <Cell >{todo.title}</Cell>
-            <Cell >{todo.details}</Cell>
-            <Cell >{todo.date}</Cell>
-            <Cell >{todo.status}</Cell>
-            <Cell>
-              <MdDeleteForever style="width:4px" />
-              <FaEdit />
+            <Cell>{todo.title}</Cell>
+            <Cell>{todo.details}</Cell>
+            <Cell>{todo.date}</Cell>
+            <Cell>{todo.status}</Cell>
+            <Cell> 
             </Cell>
+            <!-- style="width: {sizze[0]}%" 
+            style="width: {sizze[1]}%" 
+            style="width: {sizze[2]}%" 
+            style="width: {sizze[3]}%" 
+            style="width: {sizze[4]}%" 
+            style="width: {sizze[5]}%"  -->
 
           </Row>
           
@@ -54,9 +98,17 @@
 </div>
 <style>
 
-  .icons{
-    color: green !important;
-    font-size: smaller !important ;
+  .icon {
+    display: table;
+    color: red;
+    width: 32px;
+    height: 32px;
+  }
+
+  .cells{
+    text-align: left;
+    background-color: red;
+    color: blue !important;
   }
 
 </style>
